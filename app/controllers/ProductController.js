a56ec05fd85bc6fe279cb8d7ceabaf6be1239f30
tubmt20 <tubmt20@gmail.com>
2022-10-04 17:38:58 +0700

    aaaa

--- app/controllers/ProductController.js
+++ app/controllers/ProductController.js
@@ -3,11 +3,11 @@ const Product = db.products;
 const Op = db.Sequelize.Op;
 
 exports.createProduct = (req, res, next) => {
-    const { name, description, price, quantity, category } = req.body;
-    if (!name || !description || !price || !quantity || !category) {
+    const { code, name, description, price, quantity, category } = req.body;
+    if (!code || !name || !description || !price || !quantity || !category) {
         return res.json({ message: 'Please enter all the details' })
     }
-    Product.create(req.body)
+    Product.product.create(req.body)
         .then(data => {
             res.send(data);
         })
@@ -15,6 +15,67 @@ exports.createProduct = (req, res, next) => {
             res.status(500).send({
                 message:
                     err.message || "Some error occurred while creating the Product."
+            });
+        });
+}
+
+exports.createProductAttribute = (req, res, next) => {
+    const { code, name, } = req.body;
+    if (!code || !name) {
+        return res.json({ message: 'Please enter all the details' })
+    }
+    db.products.ProductAttribute.create(req.body)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Product Attribute."
+            });
+        });
+}
+
+exports.createProductAttributeValue = (req, res, next) => {
+    const { code, value, productattributeId, productId } = req.body;
+    if (!code || !value || !productattributeId || !productId) {
+        return res.json({ message: 'Please enter all the details' })
+    }
+    Product.ProductAttributesValue.create(req.body)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Product."
+            });
+        });
+}
+
+exports.showListProduct = (req, res, next) => {
+    Product.Product.findAll({
+        include: [
+            {
+                model: Product.ProductAttribute,
+                include: [
+                    {
+                        model: Product.ProductAttributesValue,
+                        as: 'values',
+                        attributes: ['value']
+                    }
+                ],
+                attributes: ['name', 'code']
+            }
+        ]
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving Products."
             });
         });
 }
